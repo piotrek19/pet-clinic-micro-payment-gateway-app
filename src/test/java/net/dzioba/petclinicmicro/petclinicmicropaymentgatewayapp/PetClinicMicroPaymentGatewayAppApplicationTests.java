194e7f72c4b0829001855921622e246333a44244
piotrek19 <dziobap@gmail.com>
2019-12-25 14:02:19 +0100

    initial commit

---
+++ src/test/java/net/dzioba/petclinicmicro/petclinicmicropaymentgatewayapp/PetClinicMicroPaymentGatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro.petclinicmicropaymentgatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMicroPaymentGatewayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
