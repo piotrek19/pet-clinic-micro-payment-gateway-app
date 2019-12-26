0faa8e8989f6ba44bfc7ead604271d89cff6ab9f
piotrek19 <dziobap@gmail.com>
2019-12-26 22:43:34 +0100

    1/ refactored to multi module project 2/ circleCi configuration added 3/ docker configuration added

---
+++ payment-gateway-web/src/main/java/net/dzioba/petclinicmicro/PetClinicMicroPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMicroPaymentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicMicroPaymentGatewayApplication.class, args);
+    }
+
+}
--- payment-gateway-web/src/test/java/net/dzioba/petclinicmicro/PetClinicMicroPaymentGatewayApplicationIT.java
+++ payment-gateway-web/src/test/java/net/dzioba/petclinicmicro/PetClinicMicroPaymentGatewayApplicationIT.java
@@ -1,10 +1,10 @@
-package net.dzioba.petclinicmicro.petclinicmicropaymentgatewayapp;
+package net.dzioba.petclinicmicro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PetClinicMicroPaymentGatewayAppApplicationTests {
+class PetClinicMicroPaymentGatewayApplicationIT {
 
     @Test
     void contextLoads() {
